89c83db059a307dcac3aca1fd4ef2b3d315bafd4
marcc1875 <mc2088@hw.ac.uk>
2026-06-29 14:55:29 +0100

    Fixed app.py to use config

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,15 @@ from config import Config
 
 app = Flask(__name__)
 
+# Load application configuration
+app.config.from_object(Config)
+
 # simple test route
 @app.route("/")
 def home():
-    return {"message": "Backend is running"}
+    return {
+        "message": "Backend is running"
+    }
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=app.config["DEBUG"])
